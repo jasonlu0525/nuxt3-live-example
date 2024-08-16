e9435ae15a499f9b706179b883749be7229db176
JasonLu <s099113302@gmail.com>
2024-08-16 16:48:28 +0800

    upload example

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+const { count, increment, decrement } = useCounter();
+
+const title = ref('計數器');
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <div class="counter">
+    <button type="button" @click="decrement">減少</button>
+    <p>{{ count }}</p>
+    <button type="button" @click="increment">增加</button>
+  </div>
+</template>
+
+
+<style scoped>
+.counter {
+  display: flex;
+  gap: 1rem;
+}
+</style>
