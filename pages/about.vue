0efe379740374dc7ca596a97fa903034e0332d01
JasonLu <s099113302@gmail.com>
2024-09-07 16:05:36 +0800

    upload example

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<script setup>
+useSeoMeta({
+  title: "關於我們",
+  ogTitle: "關於我們- Nuxt3 Day12 useSeoMeta() 與 useServerSeoMeta() 練習",
+  ogImage: "http://localhost:3000/share.jpg",
+  ogDescription:
+    "關於我們 - 透過今天的學習，將會學習到 Nuxt3 useSeoMeta() 與 useServerSeoMeta() 的使用方法",
+  description:
+    "關於我們 - 透過今天的學習，將會學習到 Nuxt3 useSeoMeta() 與 useServerSeoMeta() 的使用方法",
+});
+
+useServerSeoMeta({
+  title: "useServerSeoMeta",
+  ogTitle: "useServerSeoMeta",
+  ogImage: "http://localhost:3000/useServerSeoMeta.jpg",
+  ogDescription: "useServerSeoMeta",
+  description: "useServerSeoMeta",
+});
+</script>
+
+<template>
+  <h1>Page: about</h1>
+</template>
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<script setup>
+const title = ref("首頁");
+const theme = ref("useSeoMeta() 與 useServerSeoMeta()");
+
+useHead({
+  title,
+  titleTemplate: (title) => `${title} - Nuxt3 Day12 ${theme.value} 練習`,
+});
+
+useSeoMeta({
+  ogTitle: () => `${title.value} - Nuxt3 Day12 ${theme.value} 練習`,
+  ogImage: "http://localhost:3000/share.jpg",
+  ogDescription: () =>
+    `${title.value} - 透過今天的學習，將會學習到 Nuxt3 ${theme.value} 的使用方法`,
+  description: () =>
+    `${title.value} - 透過今天的學習，將會學習到 Nuxt3 ${theme.value} 的使用方法`,
+});
+
+useServerSeoMeta({
+  ogTitle: () => `${title.value} - Nuxt3 Day12 ${theme.value} 練習`,
+  ogImage: "http://localhost:3000/share.jpg",
+  ogDescription: () =>
+    `${title.value} - 透過今天的學習，將會學習到 Nuxt3 ${theme.value} 的使用方法`,
+  description: () =>
+    `${title.value} - 透過今天的學習，將會學習到 Nuxt3 ${theme.value} 的使用方法`,
+});
+</script>
+<template>
+  <h1>Page: index</h1>
+  <button type="button" @click="$router.push('/about')">about</button>
+</template>
+<style scoped></style>
