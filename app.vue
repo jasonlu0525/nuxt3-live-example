280c2419c5474b8b35ea20c9cfdbfb07a1b66198
JasonLu <s099113302@gmail.com>
2024-09-11 23:29:19 +0800

    upload example

---
+++ app.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+      <NuxtLink class="navbar-brand" to="/">Day14 範例</NuxtLink>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <NuxtLink class="nav-link" to="/">首頁 ( useCookie 範例 )</NuxtLink>
+          </li>
+          <li class="nav-item">
+            <NuxtLink class="nav-link" to="/register">註冊</NuxtLink>
+          </li>
+          <li class="nav-item">
+            <NuxtLink class="nav-link" to="/login">登入</NuxtLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<script setup>
+/* ------------ */
+// 建立 Cookie 的格式
+const userCookie = useCookie("user", {
+  // 建立名稱為 user 的 cookie
+  path: "/",
+  maxAge: 600,
+});
+
+// 寫入 Cookie
+userCookie.value = "bearer 123456"; // 當修改 .value 時，cookie 的值會自動被更新
+
+/* ------------ */
+
+// 讀取 cookie
+const getUserCookie = useCookie("user"); // 取得名稱為 “user” 的 cookie
+console.log(getUserCookie.value); // 會取得  bearer 123456
+
+/* ------------ */
+// 建立 cookie
+const cartCookie = useCookie("cart", {
+  path: "/",
+});
+// 寫入值
+cartCookie.value = "bearer abcde";
+
+function clearCookie() {
+  cartCookie.value = null;
+  // 或是
+  // cartCookie.value = undefined;
+}
+/* ------------ */
+</script>
+
+<template>
+  <h1>useCookie 範例</h1>
+
+  <h2>寫入 cookie</h2>
+  {{ userCookie }}
+  <h2>取得 cookie</h2>
+  {{ getUserCookie }}
+
+  <h2>清除 cookie</h2>
+  {{ cartCookie }}
+  <br />
+  <button @click="clearCookie">清除 cookie</button>
+</template>
+
+<style scoped></style>
