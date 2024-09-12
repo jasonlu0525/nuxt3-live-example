e590bb64f3a5e0e9e2aff329c7c2891e133c87ef
JasonLu <s099113302@gmail.com>
2024-09-13 00:14:22 +0800

    update example

--- pages/login.vue
+++ pages/login.vue
@@ -1,84 +1,81 @@
 <script setup>
-const userLoginObject = ref({
+const loginFormData = ref({
   email: "",
   password: "",
 });
 
 const isEnabled = ref(false);
 
-const loginAccount = async (body) => {
+const signInUser = (body) => {
   isEnabled.value = true;
-  try {
-    const { token, exp, nickname } = await $fetch("/users/sign_in", {
-      method: "POST",
-      baseURL: "https://todolist-api.hexschool.io",
-      body,
+
+  $fetch("https://todolist-api.hexschool.io/users/sign_in", {
+    method: "POST",
+    body,
+  })
+    .then(({ token, exp }) => {
+      // 設定 cookie，儲存 token 及其過期時間
+      const cookie = useCookie("auth", {
+        expires: new Date(exp * 1000),
+        path: "/",
+      });
+      cookie.value = token;
+      alert("登入成功");
+    })
+    .catch((error) => {
+      console.dir(error);
+      const { message } = error.response._data;
+      // message 有 陣列 [] 和字串 "" 兩種回應格式
+      if (Array.isArray(message)) {
+        alert(message.join("、"));
+        return;
+      }
+      alert(message);
+    })
+    .finally(() => {
+      loginFormData.value = {};
+      isEnabled.value = false;
     });
-    // 設定 cookie，儲存 token 及其過期時間
-    const cookie = useCookie("auth", {
-      expires: new Date(exp * 1000),
-      path: "/",
-    });
-    cookie.value = token;
-    alert("登入成功");
-  } catch (error) {
-    console.dir(error);
-    const { message } = error.response._data;
-    // message 有 陣列 [] 和字串 "" 兩種回應格式
-    if (Array.isArray(message)) {
-      alert(message.join("、"));
-      return;
-    }
-    alert(message);
-  } finally {
-    userLoginObject.value = {};
-    isEnabled.value = false;
-  }
 };
 </script>
 
 <template>
   <div class="container py-5">
-    <div class="row justify-content-center gy-5">
-      <div class="col-6">
-        <h2 class="h3 mb-4">登入</h2>
-        <form @submit.prevent="loginAccount(userLoginObject)">
-          <div class="mb-4">
-            <label for="loginemail"
-              >信箱 <span class="text-danger">*</span></label
-            >
-            <input
-              type="email"
-              class="form-control"
-              id="loginemail"
-              placeholder="example@gmail.com"
-              required
-              v-model="userLoginObject.email"
-            />
-          </div>
-
-          <div class="mb-4">
-            <label for="loginpassword"
-              >密碼 <span class="text-danger">*</span></label
-            >
-            <input
-              type="password"
-              class="form-control"
-              id="loginpassword"
-              placeholder="請輸入密碼"
-              required
-              v-model="userLoginObject.password"
-            />
-          </div>
-          <button
-            class="btn btn-secondary w-100"
-            :disabled="isEnabled"
-            type="submit"
-          >
-            登入
-          </button>
-        </form>
+    <div class="col-6 mx-auto">
+      <h2 class="h3 mb-4">登入</h2>
+      <div class="mb-4">
+        <label for="loginemail">信箱 <span class="text-danger">*</span></label>
+        <input
+          type="email"
+          class="form-control"
+          id="loginemail"
+          placeholder="example@gmail.com"
+          required
+          v-model="loginFormData.email"
+        />
       </div>
+
+      <div class="mb-4">
+        <label for="loginpassword"
+          >密碼 <span class="text-danger">*</span></label
+        >
+        <input
+          type="password"
+          class="form-control"
+          id="loginpassword"
+          placeholder="請輸入密碼"
+          required
+          v-model="loginFormData.password"
+        />
+      </div>
+      <button
+        class="btn btn-secondary w-100"
+        :disabled="isEnabled"
+        type="button"
+        @click="signInUser(loginFormData)"
+      >
+        登入
+      </button>
     </div>
   </div>
 </template>
--- pages/register.vue
+++ pages/register.vue
@@ -1,7 +1,8 @@
 <script setup>
 const router = useRouter();
 
-const userRegistrationObject = ref({
+// Nuxt3 Auto-imports 機制有將 ref() 自動引入，不需另外 import { ref } from 'vue';
+const registrationFormData = ref({
   email: "",
   password: "",
   nickname: "",
@@ -9,96 +10,87 @@ const userRegistrationObject = ref({
 
 const isEnabled = ref(false);
 
-const registerAccount = async (body) => {
+const createUserAccount = (body) => {
   isEnabled.value = true;
-  try {
-    await $fetch("/users/sign_up", {
-      method: "POST",
-      baseURL: "https://todolist-api.hexschool.io",
-      body,
-    });
-    /* 請求成功 response 的資訊
-    {
-      "status": true,
-      "uid": "-..."
-      }
-    */
 
-    alert("註冊成功");
-    router.push("/login");
-  } catch (error) {
-    const { message } = error.response._data;
-    // message 有 陣列 [] 和字串 "" 兩種回應格式
-    if (Array.isArray(message)) {
-      alert(message.join("、"));
-      return;
-    }
-    alert(message);
-  } finally {
-    userRegistrationObject.value = {};
-    isEnabled.value = false;
-  }
+  $fetch("https://todolist-api.hexschool.io/users/sign_up", {
+    method: "POST",
+    body,
+  })
+    .then(() => {
+      alert("註冊成功");
+      router.push("/login");
+    })
+    .catch((error) => {
+      const { message } = error.response._data;
+      // message 有陣列 [] 和字串 "" 兩種回應格式
+      if (Array.isArray(message)) {
+        alert(message.join("、"));
+        return;
+      }
+      alert(message);
+    })
+    .finally(() => {
+      registrationFormData.value = {}; // 清空註冊表單
+      isEnabled.value = false; // 解鎖按鈕
+    });
 };
 </script>
 
 <template>
   <div class="container py-5">
-    <div class="row justify-content-center gy-5">
-      <div class="col-8">
-        <h2 class="h3 mb-4">註冊</h2>
-        <form @submit.prevent="registerAccount(userRegistrationObject)">
-          <div class="mb-4">
-            <label for="nickName">暱稱<span class="text-danger">*</span></label>
-            <input
-              type="text"
-              class="form-control"
-              id="nickName"
-              placeholder="請輸入您的暱稱"
-              required
-              v-model="userRegistrationObject.nickname"
-            />
-          </div>
-          <div class="mb-4">
-            <label for="email">信箱 <span class="text-danger">*</span></label>
-            <input
-              type="email"
-              class="form-control"
-              id="email"
-              placeholder="example@gmail.com"
-              required
-              v-model="userRegistrationObject.email"
-            />
-          </div>
+    <div class="col-8 mx-auto">
+      <h2 class="h3 mb-4">註冊</h2>
 
-          <div class="mb-4">
-            <label for="password"
-              >密碼 <span class="text-danger">*</span></label
-            >
-            <input
-              type="password"
-              class="form-control"
-              id="password"
-              placeholder="請輸入密碼"
-              required
-              v-model="userRegistrationObject.password"
-            />
-          </div>
-          <div class="d-flex gap-3">
-            <button
-              class="btn btn-primary w-50"
-              type="submit"
-              :disabled="isEnabled"
-            >
-              註冊
-            </button>
-            <NuxtLink
-              to="/login"
-              class="btn btn-outline-primary w-50"
-              :class="{ disabled: isEnabled }"
-              >已經有帳號</NuxtLink
-            >
-          </div>
-        </form>
+      <div class="mb-4">
+        <label for="nickName">暱稱<span class="text-danger">*</span></label>
+        <input
+          type="text"
+          class="form-control"
+          id="nickName"
+          placeholder="請輸入您的暱稱"
+          required
+          v-model="registrationFormData.nickname"
+        />
+      </div>
+      <div class="mb-4">
+        <label for="email">信箱 <span class="text-danger">*</span></label>
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          placeholder="example@gmail.com"
+          required
+          v-model="registrationFormData.email"
+        />
+      </div>
+
+      <div class="mb-4">
+        <label for="password">密碼 <span class="text-danger">*</span></label>
+        <input
+          type="password"
+          class="form-control"
+          id="password"
+          placeholder="請輸入密碼"
+          required
+          v-model="registrationFormData.password"
+        />
+      </div>
+      <div class="d-flex gap-3">
+        <button
+          class="btn btn-primary w-50"
+          type="button"
+          @click="createUserAccount(registrationFormData)"
+          :disabled="isEnabled"
+        >
+          註冊
+        </button>
+        <NuxtLink
+          to="/login"
+          class="btn btn-outline-primary w-50"
+          :class="{ disabled: isEnabled }"
+          >已經有帳號</NuxtLink
+        >
       </div>
     </div>
   </div>
