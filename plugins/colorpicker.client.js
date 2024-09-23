fc56cd229926ee82a1656b6194afba09fdfbf580
JasonLu <s099113302@gmail.com>
2024-09-23 22:29:26 +0800

    update example

--- plugins/colorpicker.client.js
+++ plugins/colorpicker.client.js
@@ -2,17 +2,15 @@
 
 import Vue3ColorPicker from "vue3-colorpicker";
 import "vue3-colorpicker/style.css";
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(Vue3ColorPicker);
+});
 
-// export default defineNuxtPlugin((nuxtApp) => {
-//   nuxtApp.vueApp.use(Vue3ColorPicker);
-// });
-
+/* 
 // 第二種作法 : .component()  ( 二選一 )
 import { ColorPicker } from "vue3-colorpicker";
 import "vue3-colorpicker/style.css";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component("ColorPicker", ColorPicker);
 });
-/* 
-
 */
